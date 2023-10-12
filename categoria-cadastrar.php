a93197d67f8d462480dd18b09d11121d47be254f
__coelho__ <109885900+MikalROn@users.noreply.github.com>
2023-10-12 14:02:00 -0300

    feat: cadasrtro categoria

--- categoria-cadastrar.php
+++ categoria-cadastrar.php
@@ -1,1 +1,21 @@
-categoria-cadastrar.php
+<h1> Cadastrar categoria </h1>
+
+<form action="" method="POST">
+    <label> nome categoria </label>
+    <input type="text" name="nome-categoria">
+    <input type="submit" value="cadastrar">
+</form>
+
+<?php
+if (@$_SERVER['REQUEST_METHOD'] == "POST") {
+    $nome_categoria = $_POST['nome-categoria'];
+    $sql = "INSERT INTO categoria (nome_categoria) VALUES ('$nome_categoria')";
+    $resultado = mysqli_query($conn, $sql);
+    if ($resultado) {
+        echo '<script>alert("Categoria cadastrada com sucesso")</script>';
+    } else {
+        echo '<script>alert("Erro ao cadastrar categoria")</script>';
+    }
+}
+
+?>
