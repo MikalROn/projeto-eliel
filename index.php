bbac45fd83dc73782a6a303e52b990a2a70ee879
__coelho__ <109885900+MikalROn@users.noreply.github.com>
2023-10-11 22:17:04 -0300

    feat: paginas trocam no navbar

--- index.php
+++ index.php
@@ -20,11 +20,11 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            emprestimos
+            Categoria
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Listar</a></li>
-            <li><a class="dropdown-item" href="#">Cadastrar</a></li>
+            <li><a class="dropdown-item" href="?page=categoria-listar">Listar</a></li>
+            <li><a class="dropdown-item" href="?page=categoria-cadastrar">Cadastrar</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
@@ -32,8 +32,8 @@
             Livros
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Listar</a></li>
-            <li><a class="dropdown-item" href="#">Cadastrar</a></li>
+            <li><a class="dropdown-item" href="?page=livro-listar">Listar</a></li>
+            <li><a class="dropdown-item" href="?page=livro-cadastrar">Cadastrar</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
@@ -41,8 +41,8 @@
             Atendentes
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Listar</a></li>
-            <li><a class="dropdown-item" href="#">Cadastrar</a></li>
+            <li><a class="dropdown-item" href="?page=atendentes-listar">Listar</a></li>
+            <li><a class="dropdown-item" href="?page=atendentes-cadastrar">Cadastrar</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
@@ -50,8 +50,8 @@
             Usuários
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Listar</a></li>
-            <li><a class="dropdown-item" href="#">Cadastrar</a></li>
+            <li><a class="dropdown-item" href="?page=usuarios-listar">Listar</a></li>
+            <li><a class="dropdown-item" href="?page=usuarios-cadastrar">Cadastrar</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
@@ -59,8 +59,8 @@
             Emprestimo
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Listar</a></li>
-            <li><a class="dropdown-item" href="#">Cadastrar</a></li>
+            <li><a class="dropdown-item" href="?page=emprestimo-listar">Listar</a></li>
+            <li><a class="dropdown-item" href="?page=emprestimo-cadastrar">Cadastrar</a></li>
           </ul>
         </li>
       </ul>
@@ -171,6 +171,6 @@
         </div>         
     </div>
     
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
