d5e95c528344efebd10bf5019589dc4e56b6b2b9
__coelho__ <109885900+MikalROn@users.noreply.github.com>
2023-10-12 14:45:35 -0300

    salvar livro

--- livro-salvar.php
+++ livro-salvar.php
@@ -1,1 +1,47 @@
-livro-salvar.php
+<?php
+ // salvar livro
+if (@$_SERVER['REQUEST_METHOD'] == 'POST') {
+    $acao = $_POST['acao'];
+    switch($acao) {
+        case 'excluir':
+            $id_livro = $_POST['id_livro'];
+            $sql = "DELETE FROM livro WHERE id_livro = '$id_livro'";
+            $resultado = mysqli_query($conn, $sql);
+            if ($resultado) {
+                echo '<script>alert("Livro excluido com sucesso")</script>';
+            } else {
+                echo '<script>alert("Erro ao excluir livro")</script>';
+            }
+            break;
+        case 'edit':
+            $id_livro = $_POST['id_livro'];
+            $categoria = $_POST['categoria'];
+            $titulo = $_POST['titulo'];
+            $autor = $_POST['autor'];
+            $editora = $_POST['editora'];
+            $edicao = $_POST['edicao'];
+            $sql = "UPDATE livro SET categoria_id_categoria = '$categoria', titulo_livro = '$titulo', autor_livro = '$autor', editora_livro = '$editora', edicao_livro = '$edicao' WHERE id_livro = '$id_livro'";
+            $resultado = mysqli_query($conn, $sql);
+            if ($resultado) {
+                echo '<script>alert("Livro editado com sucesso")</script>';
+            } else {
+                echo '<script>alert("Erro ao editar livro")</script>';
+            }
+            break;
+        case 'create':
+            $categoria = $_POST['categoria'];
+            $titulo = $_POST['titulo'];
+            $autor = $_POST['autor'];
+            $editora = $_POST['editora'];
+            $edicao = $_POST['edicao'];
+            $sql = "INSERT INTO livro (categoria_id_categoria, titulo_livro, autor_livro, editora_livro, edicao_livro) VALUES ('$categoria', '$titulo', '$autor', '$editora', '$edicao')";
+            $resultado = mysqli_query($conn, $sql);
+            if ($resultado) {
+                echo '<script>alert("Livro cadastrado com sucesso")</script>';
+            } else {
+                echo '<script>alert("Erro ao cadastrar livro")</script>';
+            }
+            break;
+    }
+}
+?>
--- usuarios-salvar.php
+++ usuarios-salvar.php
@@ -1,1 +1,39 @@
-usuarios-salvar.php
+<?php
+// Salvar Usuarios
+
+if (@$_SERVER['REQUEST_METHOD'] == 'POST'){
+    $acao = $_POST['acao'];
+    switch($acao) {
+        case 'excluir':
+            $id_usuario = $_POST['id_usuario'];
+            $sql = "DELETE FROM usuario WHERE id_usuario = '$id_usuario'";
+            $resultado = mysqli_query($conn, $sql);
+            if ($resultado) {
+                echo '<script>alert("Usuario excluido com sucesso")</script>';
+            } else {
+                echo '<script>alert("Erro ao excluir usuario")</script>';
+            }
+            break;
+        case 'edit':
+            $id_usuario = $_POST['id_usuario'];
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+            $sql = "UPDATE usuario SET nome_usuario = '$nome', email_usuario = '$email', senha_usuario = '$senha' WHERE id_usuario = '$id_usuario'";
+            break;    
+        case 'create':
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+            $sql = "INSERT INTO usuario (nome_usuario, email_usuario, senha_usuario) VALUES ('$nome', '$email', '$senha')";
+            $resultado = mysqli_query($conn, $sql);
+            if ($resultado) {
+                echo '<script>alert("Usuario cadastrado com sucesso")</script>';
+            } else {
+                echo '<script>alert("Erro ao cadastrar usuario")</script>';
+            }
+            break;
+    
+}
+
+?>
