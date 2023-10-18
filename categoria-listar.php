75d65bc737f4ec6ea9a416c8e48541f1ae32e012
John Doe <johndoe@example.com>
2023-10-17 21:22:43 -0300

    mudando patra o padrão do professor

--- categoria-listar.php
+++ categoria-listar.php
@@ -1,10 +1,10 @@
 <h1> Lista de categorias </h1>
 
-<ul>
+<table>
     <?php
         $response = $conn->query("SELECT * FROM `categoria`");
         while($row = $response->fetch_assoc()){
             echo "<li> {$row['nome_categoria']}</li>";
         }
     ?>
-</ul>
+</table>
--- categoria-salvar.php
+++ categoria-salvar.php
@@ -1,1 +1,18 @@
-categoria-salvar.php
+<?php
+if(@$_SERVER['REQUEST_METHOD'] == 'POST'){
+    $acao = @$_POST['acao'];
+    switch ($acao) {
+        case 'cadastrar':
+            $nome_categoria = @$_POST['nome-categoria'];
+            $sql = "INSERT INTO categoria (nome_categoria) VALUES ('$nome_categoria')";
+            $resultado = $conn->query($sql);
+            if($resultado){
+                echo "<script>alert('Categoria cadastrada com sucesso!')</script>";
+                echo "<script>location.href='cadastrar-listar.php'</script>";
+            } else{
+                echo "<script>alert('Erro ao cadastrar categoria!')</script>";
+                echo "<script>location.href='cadastrar-categoria.php'</script>";
+            }
+            break;
+    }
+?>
