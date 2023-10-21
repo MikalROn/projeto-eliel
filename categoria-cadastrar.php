d2c6cae5bb4f79ee7883198111ebd00a3e228445
__coelho__ <109885900+MikalROn@users.noreply.github.com>
2023-10-21 16:47:02 -0300

    refac: forms names

--- categoria-cadastrar.php
+++ categoria-cadastrar.php
@@ -3,7 +3,7 @@
 <form action="?page=categoria-salvar" method="POST" class="m-3">
     <input type="hidden" name="acao" value="cadastrar"/>
     <label> Nome categoria </label>
-    <input type="text" name="nome-categoria" class="form-control">
+    <input type="text" name="nome_categoria" class="form-control">
     <div class="mt-3">  
         <button type="submit" value="Cadastrar" class="btn btn-success">
             Cadastrar
